607cbe21892f10523732bc54abadde3b41190331
Flavio <flavioluiscarneiro@gmail.com>
2023-11-25 19:01:38 -0300

    Initial commit

---
+++ src/main/java/com/promo/promocoesajax/PromocoesAjaxApplication.java
@@ -0,0 +1,13 @@
+package com.promo.promocoesajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocoesAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocoesAjaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promo/promocoesajax/PromocoesAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promo.promocoesajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromocoesAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
