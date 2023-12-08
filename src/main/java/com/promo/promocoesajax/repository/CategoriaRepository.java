ebb9eef0e0693ce2d95e11c90ffc4b824da960ef
Flavio <flavioluiscarneiro@gmail.com>
2023-12-08 14:36:47 -0300

    [IMP] entidade de promção e categoria. Camada de persistência.

---
+++ src/main/java/com/promo/promocoesajax/repository/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package com.promo.promocoesajax.repository;
+
+import com.promo.promocoesajax.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
---
+++ src/main/java/com/promo/promocoesajax/repository/PromocaoRepository.java
@@ -0,0 +1,8 @@
+package com.promo.promocoesajax.repository;
+
+import com.promo.promocoesajax.domain.Promocao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+}
