d90451b3d09b72311c080f73faf9c4321c91b7d0
Flavio <flavioluiscarneiro@gmail.com>
2023-12-12 20:51:04 -0300

    [IMP] submeter formulário via Ajax e salvar no banco

--- src/main/java/com/promo/promocoesajax/web/controller/PromocaoController.java
+++ src/main/java/com/promo/promocoesajax/web/controller/PromocaoController.java
@@ -1,13 +1,20 @@
 package com.promo.promocoesajax.web.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.promo.promocoesajax.domain.Categoria;
+import com.promo.promocoesajax.domain.Promocao;
 import com.promo.promocoesajax.repository.CategoriaRepository;
+import com.promo.promocoesajax.repository.PromocaoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -15,8 +22,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/promocao")
 public class PromocaoController {
 
+    private static final Logger log = LoggerFactory.getLogger(PromocaoController.class);
+
+    @Autowired
+    private PromocaoRepository promocaoRepository;
+
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @PostMapping("/save")
+    public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
+        log.info("Promoção {}", promocao.toString());
+        promocao.setDtCadastro(LocalDateTime.now());
+        promocaoRepository.save(promocao);
+        return ResponseEntity.ok().build();
+    }
 
     @ModelAttribute("categorias")
     public List<Categoria> getCategorias() {
--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -1,3 +1,33 @@
+// submit formulario para controller
+$("#form-add-promo").submit((event) => {
+    event.preventDefault();
+
+    let promo = {};
+    promo.linkPromocao = $("#linkPromocao").val();
+    promo.descricao = $("#descricao").val();
+    promo.preco = $("#preco").val();
+    promo.titulo = $("#titulo").val();
+    promo.categoria = $("#categoria").val();
+    promo.linkImagem = $("#linkImagem").attr("src");
+    promo.site = $("#site").text();
+
+    console.log("promo > ", promo);
+
+    $.ajax({
+        method: "POST",
+        url: "/promocao/save",
+        data: promo,
+        success: () => {
+            $("#alert").addClass("alert alert-success").text("Promoção cadastrada com sucesso.");
+        },
+        error: (xhr) => {
+            console.log("> error: ", xhr.responseText);
+            $("#alert").addClass("alert alert-danger").text("Não foi possível salvar esta promoção");
+        }
+    });
+})
+
+
 // capturar Meta Tags
 $("#linkPromocao").on("change", () => {
     const url = $("#linkPromocao").val();
