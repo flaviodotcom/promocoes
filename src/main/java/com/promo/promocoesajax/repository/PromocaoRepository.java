b7e9304e06f5a3436812af634b5bed80e59c7c6c
Flavio <flavioluiscarneiro@gmail.com>
2024-01-21 18:53:36 -0300

    [IMP] retorna lista de promoções após filtrar

--- src/main/java/com/promo/promocoesajax/repository/PromocaoRepository.java
+++ src/main/java/com/promo/promocoesajax/repository/PromocaoRepository.java
@@ -1,6 +1,8 @@
 package com.promo.promocoesajax.repository;
 
 import com.promo.promocoesajax.domain.Promocao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,6 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+
+    @Query("select p from Promocao p where p.site like :site")
+    Page<Promocao> findBySite(@Param("site") String site, Pageable pageable);
 
     @Query("select distinct p.site from Promocao p where p.site like %:site%")
     List<String> findSiteByTermo(@Param("site") String site);
--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -61,6 +61,32 @@ $("#autocomplete-input").autocomplete({
     }
 });
 
+$("#autocomplete-submit").on("click", function () {
+    let site = $("#autocomplete-input").val();
+    $.ajax({
+        method: "GET",
+        url: "/promocao/site/list",
+        data: {
+            site: site
+        },
+        beforeSend: function () {
+            pageNumber = 0;
+            $("#fim-btn").hide();
+            $(".row").fadeOut(400, function () {
+                $(this).empty();
+            });
+        },
+        success: function (response) {
+            $(".row").fadeIn(250, function () {
+                $(this).append(response);
+            });
+        },
+        error: function (xhr) {
+            alert(`Algo deu errado: ${xhr.status} - ${xhr.statusText}`)
+        }
+    });
+});
+
 $(document).on("click", "button[id*='likes-btn-']", function () {
     let id = $(this).attr("id").split("-")[2];
     console.log("id: ", id);
