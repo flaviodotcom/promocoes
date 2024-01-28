351da0a774819ff8dad1fe5d8e98fb70f9b0ce3b
Flavio <flavioluiscarneiro@gmail.com>
2024-01-28 19:01:32 -0300

    [IMP] tornar texto em hyperlink

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -1,3 +1,22 @@
+const Regex_Url_Str = "(https?:\\/\\/)?" // protocol
+    + "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" // domain name
+    + "((\\d{1,3}\\.){3}\\d{1,3}))" // OR ip (v4) address
+    + "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" // port and path
+    + "(\\?[;&a-z\\d%_.~+=-]*)?" // query string
+    + "(\\#[-a-z\\d_]*)?";
+
+const StringUtil = {
+    isValidUrl: (str) => new RegExp(Regex_Url_Str, "i").test(str),
+    parseHyperlinks: (str) => str.replaceAll(new RegExp(Regex_Url_Str, "gim"), "<a target=\"_blank\" rel=\"noreferrer\" href=\"$&\">$&</a>"),
+};
+
+function openLink(data, type) {
+    if (type === 'display' && StringUtil.isValidUrl(data)) {
+        return StringUtil.parseHyperlinks(data);
+    }
+    return data;
+}
+
 $(document).ready(function () {
 
     $("#table-server").DataTable({
@@ -13,9 +32,9 @@ $(document).ready(function () {
             {data: 'id'},
             {data: 'titulo'},
             {data: 'site'},
-            {data: 'linkPromocao'},
+            {data: 'linkPromocao', render: openLink},
             {data: 'descricao'},
-            {data: 'linkImagem'},
+            {data: 'linkImagem', render: openLink},
             {data: 'preco', render: $.fn.dataTable.render.number('.', ',', 2, 'R$')},
             {data: 'likes'},
             {data: 'dtCadastro'},
