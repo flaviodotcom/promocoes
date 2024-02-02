be8d9d028950a8ee075bfda2f122daf363bf7bcd
Flavio <flavioluiscarneiro@gmail.com>
2024-02-01 22:22:16 -0300

    [FIX] corrige retorno undefined para linha sem seleção e desabilita buttons

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -24,7 +24,7 @@ function dateFormat(dtCadastro) {
 
 $(document).ready(function () {
 
-    $("#table-server").DataTable({
+    const table = $("#table-server").DataTable({
         processing: true,
         serverSide: true,
         responsive: true,
@@ -52,23 +52,55 @@ $(document).ready(function () {
                 attr: {
                     id: 'btn-editar',
                     type: 'button'
-                }
+                },
+                enabled: false
             },
             {
                 text: 'Excluir',
                 attr: {
                     id: 'btn-excluir',
                     type: 'button'
-                }
+                },
+                enabled: false
             }
         ]
     });
+
+    $("#table-server thead").on('click', 'tr', function () {
+        table.buttons().disable();
+    });
+
     $("#table-server tbody").on('click', 'tr', function () {
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
+            table.buttons().disable();
         } else {
             $('tr.selected').removeClass('selected');
             $(this).addClass('selected');
+            table.buttons().enable();
         }
     });
+
+    $("#btn-editar").on('click', function () {
+        if (isSelectedRow()) {
+            let id = getPromoId();
+            alert('Botão editar selecionado ' + id);
+        }
+    });
+
+    $("#btn-excluir").on('click', function () {
+        if (isSelectedRow()) {
+            let id = getPromoId();
+            alert('Botão excluir selecionado ' + id);
+        }
+    });
+
+    function getPromoId() {
+        return table.row(table.$('tr.selected')).data().id;
+    }
+
+    function isSelectedRow() {
+        let trow = table.row(table.$('tr.selected'));
+        return trow.data() !== undefined;
+    }
 });
